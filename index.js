5ea4cac509ddbdab91abc459052423ab9716dc4c
shivanishakya-dev <shivishakya.dev@gmail.com>
2024-07-19 20:50:31 +0530

    init

---
+++ index.js
@@ -0,0 +1,20 @@
+ var tl = gsap.timeline()
+
+ tl.from("h2",{
+    y:-20,
+    opacity:0,
+    duration:1,
+    delay:0.5
+ })
+ tl.from("h4",{
+    y:-20,
+    opacity:0,
+    duration:1,
+    stagger:0.5
+ })
+ tl.from("h1",{
+    y:-20,
+    opacity:0,
+    duration:2,
+   scale:0.2
+ })
